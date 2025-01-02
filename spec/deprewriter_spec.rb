d21981727203e42f7b59dd387c4f037019398610
ohbarye <over.rye@gmail.com>
2025-01-02 14:05:23 +0900

    Add debug env

--- spec/deprewriter_spec.rb
+++ spec/deprewriter_spec.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
+class DeprewriterSpec
+  def foo
+    "foo"
+  end
+
+  extend Deprewriter
+  deprecate :foo, message: "foo is deprecated", transform_with: -> { "bar" }
+end
+
 RSpec.describe Deprewriter do
   it "has a version number" do
     expect(Deprewriter::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "deprewrites foo" do
+    expect(DeprewriterSpec.new.foo).to eq("bar")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "deprewriter"
+require "debug"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
